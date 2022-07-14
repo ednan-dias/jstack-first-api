ab5a3c1f414d1982f43ff7c4aa17fe9f2d8dd650
Ednan Dias <ednan.741@gmail.com>
2022-07-14 13:23:01 -0300

    add project to github

---
+++ mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Ednan'
+  },
+  {
+    id: 2,
+    name: 'Vera'
+  },
+  {
+    id: 3,
+    name: 'Danilo'
+  },
+  {
+    id: 4,
+    name: 'Edimar'
+  },
+]
